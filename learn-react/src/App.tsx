609e940ab07fa26fda297abe801173a9a2a624a9
OrangeO619 <1172189268@qq.com>
2026-03-09 23:56:35 +0800

    useImperativeHandle

--- learn-react/src/App.tsx
+++ learn-react/src/App.tsx
@@ -1,7 +1,72 @@
-import { useReducer,useState,useEffect,useRef } from "react"
+import { useReducer,useState,useEffect,useRef,useImperativeHandle } from "react"
+
+/* useImperativeHandle 暴露子组件的方法或属性给父组件*/
+interface ChildRef{
+  name:string
+  validate:()=>string|true
+  reset:()=>void
+}
+const Child= ({ref}:{ref:React.Ref<ChildRef>})=>{
+  const [form,setForm] = useState({
+    username:'',
+    password:'',
+    email:''
+  })
+  const validate = ()=>{
+    if(!form.username){
+      return '用户名不能为空'
+    }
+    if(!form.password){
+      return '密码不能为空'
+    }
+    if(!form.email){
+      return '邮箱不能为空'
+    }
+  }
+  const reset = ()=>{
+    setForm({
+      username:'',
+      password:'',
+      email:''
+    })
+  }
+  useImperativeHandle(ref,()=>{
+    return {
+      name:'child',
+      validate:validate,
+      reset:reset
+    }
+  })
+  return <div style={{ marginTop: '20px' }}>
+      <h3>我是表单组件</h3>
+      <input value={form.username} onChange={(e) => setForm({ ...form, username: e.target.value })} placeholder='请输入用户名' type="text" />
+      <input value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} placeholder='请输入密码' type="text" />
+      <input value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} placeholder='请输入邮箱' type="text" />
+   </div>
+}
+export function App() {
+   const childRef = useRef<ChildRef>(null)
+   const showRefInfo = () => {
+      console.log(childRef.current)
+   }
+   const submit = () => {
+      const res = childRef.current?.validate()
+      console.log(res)
+   }
+   return (
+      <div>
+         <h2>我是父组件</h2>
+         <button onClick={showRefInfo}>获取子组件信息</button>
+         <button onClick={() => submit()}>校验子组件</button>
+         <button onClick={() => childRef.current?.reset()}>重置</button>
+         <hr />
+         <Child ref={childRef}></Child>
+      </div>
+   );
+}
 
 /* useRef */
-export function App(){
+/* export function App(){
   const timer=useRef<NodeJS.Timeout | null>(null)
   const [count,setCount] = useState(0)
   const handleClick = ()=>{
@@ -22,7 +87,7 @@ export function App(){
       <button onClick={handleEnd}>结束计数</button>
     </div>
   )
-}
+} */
 
 /* useEffect */
 /* interface UserData{
