5b5aea0fdd3adb7f78d160d4f0c00bea1d555a8a
OrangeO619 <1172189268@qq.com>
2026-03-17 01:13:59 +0800

    记账本

--- react-bill/src/index.js
+++ react-bill/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 导入定制主题文件
+import './theme.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-
-    <App />
-
-);
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+)
 
 
