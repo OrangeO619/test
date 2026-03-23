e1cfc9f7c60db1025d0e15b01fe355fa842939b2
OrangeO619 <1172189268@qq.com>
2026-03-23 23:50:59 +0800

    scope讲解

--- lesson1/面试题/async.js
+++ lesson1/面试题/async.js
@@ -15,3 +15,17 @@ const sleep= (t,callback)=>{
 }
 
 test()
+
+//Generator函数：可以暂停，在需要时恢复执行。
+function* gen() {
+  console.log(1);
+  yield 2;
+  console.log(3);
+  yield 4;
+  console.log(5);
+}
+
+const g = gen();
+console.log(g.next());  // 输出：1，返回 { value: 2, done: false }
+console.log(g.next());  // 输出：3，返回 { value: 4, done: false }
+console.log(g.next());  // 输出：5，返回 { value: undefined, done: true }
--- lesson1/面试题/scope.js
+++ lesson1/面试题/scope.js
@@ -9,8 +9,40 @@ function fn(){
 console.log(a1)
 4.块级作用域  for(){}
 while(){}*/
-{
+/* {
     var b1 = '块级作用域var'
     let b2 = '块级作用域let'
 }
-console.log(b2)
+console.log(b2) */
+
+//lexical scope 词法作用域:当前作用域=>
+// let value = 10
+
+function lexicalFn2() {
+    let value = '张三'
+    function lexicalFn1() {
+        console.log(value)
+        return 2
+    }
+    return lexicalFn1
+}
+/* let res = lexicalFn2()
+console.log(res()) */
+//ReferenceError: value is not defined
+//res导致了持久闭包 
+//短暂闭包：
+console.log(lexicalFn2()())
+
+function make() {
+  let n = 0;
+  return () => ++n;
+}
+const c1 = make();
+const c2 = make();
+c1() 1
+c1() 2
+c1() 3
+c2() 1
+//闭包的作用
+1.数据的私有化
+2.currying 
