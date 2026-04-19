02e0a764f3b23f8dfc67ca70fcaabbcea5ff30ea
OrangeO619 <1172189268@qq.com>
2026-04-19 23:59:30 +0800

    二分查找

---
+++ LeetCode_Hot100/BinarySearch/searchInsert.js
@@ -0,0 +1,25 @@
+/* 
+搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left=0
+    let right=nums.length-1
+    while(left<=right){
+        let mid = Math.floor((left+right)/2)
+        if(target>nums[mid]){
+            left=mid+1
+        }else if(target<nums[mid]){
+            right=mid-1
+        }else{
+            return mid
+        }
+    }
+    return left
+};
