8180e5b630359370f3f735f3c4dbd800d665a234
OrangeO619 <1172189268@qq.com>
2026-04-27 23:57:52 +0800

    行为场景题

--- lesson2/project/server.js
+++ lesson2/project/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
-app.get('/',(request,response)=>{
+/* app.get('/',(request,response)=>{
     response.json({
     ok: true
   });
-})
+}) */
+app.use(express.static("public"))
 app.listen(PORT,()=>{
     console.log('正在运行4000端口')
 }) 
+
